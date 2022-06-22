36ccf702c12213d08113d704070dcbc39af3051e
Alan S. Ferreira <alansferreira1984@gmail.com>
2022-06-22 13:31:31 -0300

    fix: handle command helps

--- packages/cli/src/main.ts
+++ packages/cli/src/main.ts
@@ -7,7 +7,10 @@ import { resolve } from 'path'
 const packageJson = JSON.parse(
   readFileSync(resolve(__dirname, '../package.json')).toString()
 )
-const { version } = packageJson
+const {
+  version,
+  pkg: { assemblyName }
+} = packageJson
 
 interface IRenderOptions {
   input?: string
@@ -25,8 +28,31 @@ const renderCommand = (options: IRenderOptions) => {
   // console.log(options)
 }
 const program = new Command()
+program.version(version)
+program.name(assemblyName)
+program.exitOverride((error) => {
+  console.error(program.helpInformation())
+  process.exit(error.exitCode)
+})
+program.configureHelp({
+  sortSubcommands: true
+})
+
+function commandExitOverride(commandName: string) {
+  return (error) => {
+    const cmd = program.commands.find((c) => c.name() === commandName)
+    if (!cmd) {
+      console.error(program.helpInformation())
+    } else {
+      console.error(cmd.helpInformation())
+    }
+    process.exit(error.exitCode)
+  }
+}
+
 program
   .command('render')
+  .exitOverride(commandExitOverride('render'))
   .option('-i, --input <input>', 'Input template folder', './template')
   .option('-o, --output <output>', 'Output processed folder', './output')
   .option(
@@ -52,10 +78,9 @@ program
 
 // console.log(process.argv)
 
-program.version(version)
-program.exitOverride((error) => {
-  console.error(program.helpInformation())
-  process.exit(error.exitCode)
-})
-
-program.parse(process.argv, { from: 'node' })
+try {
+  program.parse(process.argv, { from: 'node' })
+} catch (error) {
+  console.info(program.helpInformation())
+  console.error(error)
+}
